323c274889c7ce707558a3f0ade2c96e0e4ba0e3
sandeepsuman1911@gmail.com <sandyn7890@gmail.com>
2024-04-26 19:00:30 +0530

    adding some function from main branch

--- file_1.c
+++ file_1.c
@@ -12,11 +12,18 @@ int main() {
     printf("%d + %d = %d", number1, number2, sum);
     return 0;
 }
+<<<<<<<<<HEAD
+int addfunc(int a, int b)
+{
+	int c = a+b;
+	return c;
+=======	
 //function added in branch1
-int func_add(int a, int c)
+int func_add(int a, int b)
 {
 	// i am in main branch
 	// i am making this changes in 
 	// one more change 
-	return a+c;
+	return a+b;
+>>>>>>> branch1	
 }
